ba7a02ae3c3642fe00b609dbaed40d2212c80c57
Manuela Andrade <100206471+Manuela-Andrade@users.noreply.github.com>
2023-04-17 20:25:27 -0300

    Add files via upload

---
+++ DAO/UsuarioDAO.js
@@ -0,0 +1,20 @@
+import db from "../infra/db.js";
+
+class UsuarioDAO {
+    
+    static listar(){
+
+        const query = "SELECT * FROM USUARIOS";
+
+        return new Promise((resolve, reject) => {
+            db.all(query, (err, rows) => {
+                if(err){
+                    reject(err);
+                }
+                resolve(rows);
+            });
+        });
+    };
+};
+
+export default UsuarioDAO
